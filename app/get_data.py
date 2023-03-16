ec2f465d16c363a865a0db1fb5d22d50822f9c8e
yellowfox <yellowfox.star.is@gmail.com>
2023-03-16 23:12:44 +0100

    Started parsing of networks

--- app/get_data.py
+++ app/get_data.py
@@ -3,6 +3,7 @@ import copy
 import search
 import json
 import objects
+import operations
 from qpylib import qpylib
 
 event_source_type_name = "Mikrotik RouterOS"
@@ -64,9 +65,31 @@ def get_routers():
     return result
 
 
+def get_networks(router_id):
+    # get information in which network can be searched
+    network_columns = ['sourceip', 'sourcev6', 'identityip']
+    query = f'SELECT {operations.str_with_delimiter(network_columns,",")} FROM events WHERE logsourceid = {router_id} LAST 7 DAYS'
+    search_results = search.search(query)
+
+    # parse information into more workable format
+    ip4_empty = ['0.0.0.0']
+    ip6_empty = ['0:0:0:0:0:0:0:0']
+    sourceip_list = []
+
+    # TODO generalize it, so it can easily be used for other possible network columns
+    for item in search_results:
+        if ip4_empty.count(item['sourceip']) == 0:
+            sourceip_list.append(item['sourceip'])
+
+
+    return None
+
+
+
 if __name__ == "__main__":
     routers = get_routers()
     print(routers)
-    search_results = search.search(f"SELECT * FROM events WHERE logsourceid = {routers[0]['id']} LAST 7 DAYS")
-    print(json.dumps(search_results, indent=4))
+    # search_results = search.search(f"SELECT * FROM events WHERE logsourceid = {routers[0]['id']} LAST 7 DAYS")
+    # print(json.dumps(search_results, indent=4))
     # print(get_all())
+    print(get_networks(routers[0]["id"]))
---
+++ app/operations.py
@@ -0,0 +1,2 @@
+def str_with_delimiter(list0, delimiter):
+    return str(delimiter).join(str(x) for x in list0)
